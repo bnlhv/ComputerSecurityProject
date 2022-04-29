c23b324032281205d46c4916af19d06b1b2c4469
Ben Lahav <benlahav1990@gmail.com>
2022-04-29 22:10:05 +0300

    Added migrations for sectors and data package

--- communicationLTD/client/admin.py
+++ communicationLTD/client/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Client
+from .models import *
 
 admin.site.register(Client)
+admin.site.register(DataPackage)
+admin.site.register(Sectors)
--- communicationLTD/client/models.py
+++ communicationLTD/client/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models import CharField
 
 
-class Client(models.Model):
+class DataPackage(models.Model):
     class DataPackageOptions(models.TextChoices):
         _2GB = "2GB"
         _5GB = "5GB"
@@ -10,21 +10,26 @@ class Client(models.Model):
         _50GB = "50GB"
         _100GB = "100GB"
 
-    full_name = models.CharField(max_length=50,
-                                 default="default_name",
-                                 null=True)
-    email = models.EmailField(max_length=50,
-                              null=True)
-    data_package = models.CharField(max_length=5,
+    package_size = models.CharField(max_length=5,
                                     choices=DataPackageOptions.choices,
                                     default=DataPackageOptions._2GB)
-    date_created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self) -> CharField:
-        return self.full_name
 
 
 class Sectors(models.Model):
     name = models.CharField(max_length=50,
                             default="default_sector",
                             null=True)
+
+
+class Client(models.Model):
+    full_name = models.CharField(max_length=50,
+                                 default="default_name",
+                                 null=True)
+    email = models.EmailField(max_length=50,
+                              null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    data_package = models.ForeignKey(DataPackage, null=True, on_delete=models.SET_NULL)
+    sectors = models.ManyToManyField(Sectors)
+
+    def __str__(self) -> CharField:
+        return self.full_name
