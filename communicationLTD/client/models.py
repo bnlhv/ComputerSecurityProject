a7b512b05279249da4082dd33d88055ee37c1182
Ben Lahav <benlahav1990@gmail.com>
2022-04-29 17:44:45 +0300

    Added data package model and sectors model

--- communicationLTD/client/models.py
+++ communicationLTD/client/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db.models import CharField
 
-# Create your models here.
+
+class Client(models.Model):
+    class DataPackageOptions(models.TextChoices):
+        _2GB = "2GB"
+        _5GB = "5GB"
+        _10GB = "10GB"
+        _50GB = "50GB"
+        _100GB = "100GB"
+
+    full_name = models.CharField(max_length=50,
+                                 default="default_name",
+                                 null=True)
+    email = models.EmailField(max_length=50,
+                              null=True)
+    data_package = models.CharField(choices=DataPackageOptions.choices,
+                                    default=DataPackageOptions._2GB)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> CharField:
+        return self.full_name
+
+
+class Sectors(models.Model):
+    name = models.CharField(max_length=50,
+                            default="default_sector",
+                            null=True)
