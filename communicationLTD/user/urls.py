8c1e4ae8226e8db82585c6939f0f4146cb526991
Ben Lahav <benlahav1990@gmail.com>
2022-04-29 16:35:12 +0300

    Added footer.html

--- communicationLTD/user/urls.py
+++ communicationLTD/user/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.login),
     path('login/', views.login),
     path('register/', views.register),
     path('forgot_password/', views.forgot_password),
