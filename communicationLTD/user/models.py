71b7f2a6eb03021995d107c9219c6c25d721e6a6
kokolight <tsegal97@gmail.com>
2022-05-03 22:15:13 +0300

    added user model

--- communicationLTD/user/models.py
+++ communicationLTD/user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models import CharField
 
 # Create your models here.
+class User(models.Model):
+
+    GENDER_CHOICES = (("1", "male"), ("2", "female"), ("3", "other"))
+    PLAN_CHOICES = (("1", "2GB"), ("2", "5GB"), ("3", "10GB"), ("4", "50GB"), ("5", "100GB"))
+
+    _id = models.AutoField
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birthday = models.DateField()
+    gender = models.TextField(choices=GENDER_CHOICES)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=30)
+    plan = models.TextField(choices=PLAN_CHOICES)
+
+    def __str__(self) -> CharField:
+        return self._id
