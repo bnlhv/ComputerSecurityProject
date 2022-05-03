66d72c400273e906ca8090cc02d1a779292f6863
kokolight <tsegal97@gmail.com>
2022-05-03 23:09:54 +0300

    added login functionality

--- communicationLTD/user/forms.py
+++ communicationLTD/user/forms.py
@@ -1,13 +1,12 @@
 from django.forms import ModelForm
 from django import forms
+
 # from .models import User
-from django.contrib.auth.forms import UserCreationForm 
-from django.contrib.auth.models import User 
-
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ["username", "email", "password1", "password2"]
--- communicationLTD/user/views.py
+++ communicationLTD/user/views.py
@@ -1,27 +1,44 @@
+import imp
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.forms import inlineformset_factory
+from django.contrib import messages
 from .forms import CreateUserForm
 
 
 def login(request) -> HttpResponse:
-    """ :return: Login html """
+    """:return: Login html"""
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("user/loginsss")
+        else:
+            messages.error(request, "Invalid credentials")
+
+    context = {}
     return render(request, "user/login.html")
 
 
 def register(request) -> HttpResponse:
-    """ :return: Register html """
+
+    """:return: Register html"""
     form = CreateUserForm()
 
-    if request.method == 'POST':
-        form = CreateUserForm(requetst.POST)
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
 
-    context = {'form': form}
+    context = {"form": form}
     return render(request, "user/register.html")
 
 
 def forgot_password(request) -> HttpResponse:
-    """ :return: Forgot Password html """
+    """:return: Forgot Password html"""
     return render(request, "user/forgot_password.html")
