28c28eccea815fed8be259707d64a934cfd5e571
Bar Nehemia <bar@lightricks.com>
2022-05-05 21:54:07 +0300

    user: Add `ChangePassowrd`.
    In addition, change the length of valid password to 10.

--- communicationLTD/user/urls.py
+++ communicationLTD/user/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("login/", views.login_page, name="login"),
     path("logout/", views.logout_page, name="logout"),
     path("register/", views.register_page, name="register"),
+    path("change_password/", views.ChangePassword.as_view(), name="change_password"),
 ]
--- communicationLTD/user/views.py
+++ communicationLTD/user/views.py
@@ -1,11 +1,27 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
 
 from .forms import CreateUserForm
 
+class ChangePassword(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy("clients")
+    template_name = "user/change_password.html"
+
+    @method_decorator(sensitive_post_parameters())
+    @method_decorator(csrf_protect)
+    @method_decorator(login_required(login_url='login'))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 def login_page(request) -> HttpResponse:
     """:return: Login html"""
@@ -35,7 +51,7 @@ def register_page(request) -> HttpResponse:
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, "User registered succesfully")
+            messages.success(request, "User registered successfully")
             return redirect("login")
 
     context = {"form": form}
