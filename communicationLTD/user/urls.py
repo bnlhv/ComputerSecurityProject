006a2e4c0a2c37ed800e87c4b878c8d3edb2825c
Ben Lahav <benlahav1990@gmail.com>
2022-05-07 20:22:16 +0300

    Updated reset password

--- communicationLTD/user/urls.py
+++ communicationLTD/user/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
@@ -8,4 +10,8 @@ urlpatterns = [
     path("logout/", views.logout_page, name="logout"),
     path("register/", views.register_page, name="register"),
     path("change_password/", views.ChangePassword.as_view(), name="change_password"),
+    path("reset_password/", auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
