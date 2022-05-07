8794d62d53b8c2b5ebb16bd44cf262ddf9b934a1
Ben Lahav <benlahav1990@gmail.com>
2022-05-07 21:42:43 +0300

    Changed all views from Django template to our template

--- communicationLTD/communicationLTD/settings.py
+++ communicationLTD/communicationLTD/settings.py
@@ -149,4 +149,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'communicationltdforgotpass@gmail.com'
-EMAIL_HOST_PASSWORD = "Aa123456!"
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
