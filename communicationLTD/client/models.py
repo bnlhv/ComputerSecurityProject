72dc7dd37dc9312b778300b14fa0c1a49e848fe3
Ben Lahav <benlahav1990@gmail.com>
2022-05-13 15:17:41 +0300

    Demontration of sql injection

--- communicationLTD/client/models.py
+++ communicationLTD/client/models.py
@@ -1,5 +1,25 @@
-from django.db import models
+from datetime import datetime
+from typing import List, Any
+
+from django.db import models, connection
 from django.db.models import CharField
+
+
+def create_client_with_sqli_demonstration(full_name: str,
+                                          email: str,
+                                          data_package: str,
+                                          sectors: List) -> Any:
+    """ Bypass the ORM and execute raw sql query directly to th database """
+    create_query = "INSERT INTO clients(full_name, email, date_created, " \
+                   "data_package) VALUES (%s, %s, %s, %s);"
+    fetch_query = "SELECT * FROM clients WHERE full_name = %s;"
+    with connection.cursor() as cursor:
+        cursor.execute(create_query, [full_name, email, data_package, datetime.now()])
+        cursor.execute(fetch_query, [full_name])
+        row = cursor.fetchall()
+        print(row)
+
+    return row
 
 
 class Sectors(models.Model):
@@ -28,7 +48,12 @@ class Client(models.Model):
     data_package = models.CharField(max_length=5,
                                     null=True,
                                     choices=PACKAGES)
-    sectors = models.ManyToManyField(Sectors, related_name="sectors")
+    sectors = models.ManyToManyField(Sectors,
+                                     default=None,
+                                     related_name="sectors")
+
+    class Meta:
+        db_table = "clients"
 
     def __str__(self) -> CharField:
         return self.full_name
